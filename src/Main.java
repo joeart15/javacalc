0756ca4033274f5c59ab5dc4cee1270d5273d738
Josue Arteaga <85711055+joeart15@users.noreply.github.com>
2025-03-25 10:52:02 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+package com.github.joeart15.javacalc;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Calculator calculator = new Calculator();
+
+        System.out.println("5 + 3 = " + calculator.add(5, 3));
+        System.out.println("5 - 3 = " + calculator.subtract(5, 3));
+        System.out.println("5 * 3 = " + calculator.multiply(5, 3));
+        System.out.println("6 / 2 = " + calculator.divide(6, 2));
+        System.out.println("7 % 3 = " + calculator.modulus(7, 3));
     }
 }
